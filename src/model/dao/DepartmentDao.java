89508216ac283d837c66acc13796cf141329bd5d
Mestre Supremo <nelsonred100@gmail.com>
2022-07-12 19:19:15 -0300

    DaoJDBC

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,5 +1,14 @@
 package model.dao;
 
-public interface DepartmentDao {
+import java.util.List;
 
+import model.entities.Departmet;
+
+public interface DepartmentDao {
+	void insert(Departmet dp );
+	void update(Departmet dp );
+	void deleteById(Integer id);
+	Departmet findById(Integer id);
+	List<Departmet> findAll();
+	
 }
