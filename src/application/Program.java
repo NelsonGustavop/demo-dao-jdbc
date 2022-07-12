48700e8d67e1465bfb56f128ce43344b8d3a35ae
Mestre Supremo <nelsonred100@gmail.com>
2022-07-11 21:43:25 -0300

    HEHEboy

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departmet;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Departmet dp = new Departmet(1, "books");
-		System.out.println(dp);
+		Seller sl = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, dp);
+		System.out.print(dp +","+ sl);
 	}
 
 }
