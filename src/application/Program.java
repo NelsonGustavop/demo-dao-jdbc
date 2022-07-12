96bcb452a3d4bbc910cf22647d66ff3adf431d58
Mestre Supremo <nelsonred100@gmail.com>
2022-07-11 21:26:35 -0300

    MYbestoFRENDO

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import model.entities.Departmet;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Departmet dp = new Departmet(1, "books");
+		System.out.println(dp);
+	}
+
+}
