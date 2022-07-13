6e9350a953da676b6162064e09ea6169e20ca935
Mestre Supremo <nelsonred100@gmail.com>
2022-07-12 22:21:00 -0300

    Ta foda

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,29 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.dao.SellerDao;
+import model.entities.Departmet;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		SellerDao sld = DaoFactory.createSellerDao();
 		System.out.println("=== Test 1: seller findById ===");
 		Seller sl = sld.findById(3);
+		System.out.println("\n=== Test 2: seller findByDepartment ===");
+		Departmet dp = new Departmet(2, "hungay");
+
+		List<Seller> list = SellerDao.findByDepartmet(dp);
+
+		for (Seller sl1 : list) {
+			System.out.println(sl1);
+		}
+
 		System.out.println(sl);
 	}
 
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -2,6 +2,7 @@ package model.dao;
 
 import java.util.List;
 
+import model.entities.Departmet;
 import model.entities.Seller;
 
 public interface SellerDao {
@@ -10,4 +11,8 @@ public interface SellerDao {
 	void deleteById(Integer id);
 	Seller findById(Integer id);
 	List<Seller> findAll();
+static List<Seller> findByDepartmet(Departmet dp) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
