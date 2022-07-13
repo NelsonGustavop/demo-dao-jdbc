5c4cc528f43af375114ed33d311e89a6ad6d7456
Mestre Supremo <nelsonred100@gmail.com>
2022-07-12 21:46:48 -0300

    Esse deu ruim(Corrigir depois)

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,16 @@
 package application;
 
-import java.util.Date;
-
-import model.entities.Departmet;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Departmet dp = new Departmet(1, "books");
-		Seller sl = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, dp);
-		System.out.print(dp +","+ sl);
+		
+		SellerDao sld = DaoFactory.createSellerDao();
+		Seller sl = sld.findById(3);
+		System.out.println(sl);
 	}
 
 }
